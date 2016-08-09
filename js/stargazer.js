b100254a490e7e542e67e4b3a6cc1f0fcf3e6cfb
jesse houghton <OiseauNoir>
2016-08-09 18:57:19 -0500

    added jsonp functionality

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,5 +1,22 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
+$(document).ready(function() {
+  $('#search').on('submit', function() {
+    var name = $('#search-name').val();
+    //console.log(name)
+      $.ajax({
+        url: 'http://www.strudel.org.uk/lookUP/json/?name='+name,
+        dataType: "jsonp",
+        jsonpCallback: "logResults"
+      })
+    // .done(function(data) {
+    //   console.log(data);
+    // })
+      .fail(function() {
+        alert('error');
+      })
+    return false;
+  })
+});
+
+function logResults(json){
+  console.log('it ran', json);
 }
