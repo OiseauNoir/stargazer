ca2ee04d06d9ed698e8401292153c70cc0e69438
jesse houghton <OiseauNoir>
2016-08-10 15:14:26 -0500

    added error response

--- js/stargazer.js
+++ js/stargazer.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   $('#search').on('submit', function() {
     var name = $('#search-name').val();
+
+
+
     //console.log(name)
       $.ajax({
         url: 'http://www.strudel.org.uk/lookUP/json/?name='+name,
@@ -10,14 +13,18 @@ $(document).ready(function() {
     // .done(function(data) {
     //   console.log(data);
     // })
-      .fail(function() {
-        alert('error');
-      })
+      // .fail(function() {
+      //   alert('error');
+      // })
     return false;
   })
 });
 
-function logResults(json){
-  console.log(json);
-  $('#preview').html('<img src="' + json.image.src + '">')
+function logResults(json){   //console.log(json);
+  console.log(json)
+  if (json.image) {
+    $('#preview').html('<img src="' + json.image.src + '">')
+  } else {
+    $('#preview').html(json.message)
+  }
 }
